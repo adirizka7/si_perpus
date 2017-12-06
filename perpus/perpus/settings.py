eb6e600d3f2ea94ebb0b3c074684c98d441decb3
jodhi <jodhilesmana@gmail.com>
2017-12-06 16:24:28 +0700

    work url

--- perpus/perpus/settings.py
+++ perpus/perpus/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'perpus.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'proses/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
