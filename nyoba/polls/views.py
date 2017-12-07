f5cf1d5e97b4dd3693e96166a427588930b67e70
jodhi <jodhilesmana@gmail.com>
2017-12-07 14:17:52 +0700

    big ui modification

--- nyoba/polls/views.py
+++ nyoba/polls/views.py
@@ -27,6 +27,18 @@ class cek_status(generic.ListView):
 	model = Mahasiswa
 	template_name = 'polls/cek_status.html'
 
+class tambah_syarat(generic.ListView):
+	model = Mahasiswa
+	template_name = 'polls/tambah_syarat.html'
+
+class rekap_data(generic.ListView):
+	model = Mahasiswa
+	template_name = 'polls/rekap_data.html'
+
+class import_csv(generic.ListView):
+	model = Mahasiswa
+	template_name = 'polls/import_csv.html'
+
 
 class Searched(generic.ListView):
 	template_name = 'polls/temuan.html'
